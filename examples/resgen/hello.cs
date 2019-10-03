c7f934549c9ea09c58dd3e2014ba01c362c015cf
jrbeverly <Jonathan.Beverly@D2L.com>
2019-10-03 13:10:24 -0400

    Simplify example to single resx

--- examples/resgen/hello.cs
+++ examples/resgen/hello.cs
@@ -4,13 +4,6 @@ using System.Globalization;
 namespace Hello {
   public static class Program {
     public static void Main() {
-      Console.WriteLine("The current culture is {0}.", CultureInfo.CurrentUICulture.Name);
-      Console.WriteLine("Message: {0}", Strings.Hello);
-
-      // Change the current culture to fr-CA.
-      Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("fr-CA");
-
-      Console.WriteLine("The current culture is {0}.", CultureInfo.CurrentUICulture.Name);
       Console.WriteLine("Message: {0}", Strings.Hello);
     }
   }
